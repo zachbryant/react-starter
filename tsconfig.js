fa39a74a7dad13fb383cae33934c9140fafba976
Zach Bryant <zdb282@gmail.com>
2021-02-08 01:41:05 -0800

    subfolder aliases

--- tsconfig.js
+++ tsconfig.js
@@ -1,58 +1,64 @@
-const fs = require( 'fs' );
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fs = require('fs');
 
 const baseUrl = 'src';
 
-// Import aliases like `import("@Public/img/myasset.png")`
-function generateIntellisensePaths() {
-    const paths = {
-        '@/*': ['./*'],
-        '~/*': ['../*'],
-    };
-
-    const dir = await fs.promises.opendir( baseUrl );
-    for await ( const dirent of dir ) {
-        if ( dirent.isDirectory ) {
-            let name = dirent.name;
-            paths[`@${name}/*`] = `${name}/*`
-            paths[`@${name}/`] = `${name}/index`
-            paths[`@${name}`] = `${name}/index`
-        }
-    }
-    return paths;
-}
-
-const exports = {
-    compilerOptions: {
-        target: 'es2020',
-        lib: ['dom', 'dom.iterable', 'esnext'],
-        allowJs: true,
-        skipLibCheck: true,
-        esModuleInterop: true,
-        allowSyntheticDefaultImports: true,
-        noImplicitReturns: true,
-        strict: true,
-        forceConsistentCasingInFileNames: true,
-        module: 'es2020',
-        moduleResolution: 'node',
-        resolveJsonModule: true,
-        isolatedModules: true,
-        noEmit: false,
-        jsx: 'react',
-        newLine: 'lf',
-        baseUrl,
-        paths: generateIntellisensePaths(),
-    },
-    include: ['src'],
-    exclude: [
-        'lib',
-        'build',
-        'node_modules',
-        'scripts',
-        'webpack',
-        'jest',
-        'tests',
-        'templates',
-    ],
+module.exports = {
+	compilerOptions: {
+		target: 'es2020',
+		lib: ['dom', 'dom.iterable', 'esnext'],
+		allowJs: true,
+		skipLibCheck: true,
+		esModuleInterop: true,
+		allowSyntheticDefaultImports: true,
+		noImplicitReturns: true,
+		strict: true,
+		forceConsistentCasingInFileNames: true,
+		module: 'es2020',
+		moduleResolution: 'node',
+		resolveJsonModule: true,
+		isolatedModules: true,
+		noEmit: false,
+		jsx: 'react',
+		newLine: 'lf',
+		baseUrl,
+		paths: generateIntellisensePaths(),
+	},
+	include: ['src'],
+	exclude: ['lib', 'build', 'node_modules', 'scripts', 'webpack', 'jest', 'tests', 'templates'],
 };
 
-module.exports = exports;
+// Import aliases like `import("@Public/img/myasset.png")`
+function generateIntellisensePaths() {
+	const paths = {
+		'@/*': ['./*'],
+		'~/*': ['../*'],
+	};
+	return getPathsFromDir(paths, '', 0, 1);
+}
+
+// Get a list of sub-directories
+function getDirectories(source) {
+	return fs
+		.readdirSync(source, { withFileTypes: true })
+		.filter((_) => _.isDirectory())
+		.filter((_) => _ !== 'dist')
+		.map((_) => _.name);
+}
+
+function getPathsFromDir(paths, pathString, level, maxLevel) {
+	if (level > maxLevel) return;
+
+	for (const name of getDirectories(`${baseUrl}/${pathString}`)) {
+		const newPathString = [pathString, name].filter(Boolean).join('/');
+		const index = `${newPathString}/index`;
+		paths[`@${name}/*`] = [`${newPathString}/*`, index];
+		//paths[`@${name}/`] = [index];
+		paths[`@${name}`] = [index];
+		paths = {
+			...paths,
+			...getPathsFromDir(paths, newPathString, level + 1, maxLevel),
+		};
+	}
+	return paths;
+}
