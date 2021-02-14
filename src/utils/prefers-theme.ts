75d42f1f69f6abcf9ec2c797930ce6a63a02b806
Zach <32501288+zachbryant@users.noreply.github.com>
2021-02-13 17:38:39 -0800

    Update prefers-theme.ts

--- src/utils/prefers-theme.ts
+++ src/utils/prefers-theme.ts
@@ -12,17 +12,21 @@ export function mediaSchemeMatches(theme: string) {
 	);
 }
 
-export function applyThemeToSelector(selector: string, theme: string) {
+export function applyThemeToSelector(selector: string, theme: string, attributeName = 'theme') {
 	const elems = document.querySelectorAll(selector);
 	for (const e of elems) {
-		e.setAttribute('theme', theme);
+		e.setAttribute(attributeName, theme);
 	}
 }
 
-export function applyPreference(selector: string) {
+export function applyThemePreference(selector: string, attributeName = 'theme') {
 	if (prefersDark() || !prefersLight()) {
-		applyThemeToSelector(selector, 'dark');
+		applyThemeToSelector(selector, 'dark', attributeName);
 	} else {
-		applyThemeToSelector(selector, 'light');
+		applyThemeToSelector(selector, 'light', attributeName);
 	}
+}
+
+export function getPreferredTheme() {
+	return prefersLight() ? 'light' : 'dark';
 }
