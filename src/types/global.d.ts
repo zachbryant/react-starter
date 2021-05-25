04111d6d32cc5c48e4d275fd71bd5f2be9570138
Zach Bryant <zdb282@gmail.com>
2021-05-25 01:59:45 -0700

    touchups

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -3,3 +3,17 @@ declare module '*.scss' {
 	const content: { [className: string]: string };
 	export = content;
 }
+declare module '*.svg' {
+	const content: any;
+	export default content;
+}
+
+declare module '*.jpg' {
+	const content: any;
+	export default content;
+}
+
+declare module '*.png' {
+	const content: any;
+	export default content;
+}
