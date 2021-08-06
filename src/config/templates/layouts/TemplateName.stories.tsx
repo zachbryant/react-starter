d660c9a434e86fe29566a9d592423520127f3c1e
Zach Bryant <zdb282@gmail.com>
2021-08-06 03:23:00 -0700

    templating, remove fake component

--- src/config/templates/layouts/TemplateName.stories.tsx
+++ src/config/templates/layouts/TemplateName.stories.tsx
@@ -1,7 +1,17 @@
 import userEvent from '@testing-library/user-event';
 import { TemplateName, TemplateNameProps } from './TemplateName';
 
-export default { component: TemplateName, title: 'Layouts/TemplateName' };
+export default {
+	component: TemplateName,
+	title: 'Layouts/TemplateName',
+	// Learn about controls https://storybook.js.org/docs/react/essentials/controls
+	argTypes: {
+		variant: {
+			options: ['primary', 'secondary'],
+			control: { type: 'radio' },
+		},
+	},
+};
 export const Default = {
 	args: {},
 	render: undefined,
--- src/config/templates/pages/TemplateName.stories.tsx
+++ src/config/templates/pages/TemplateName.stories.tsx
@@ -1,7 +1,17 @@
 import userEvent from '@testing-library/user-event';
 import { TemplateName, TemplateNameProps } from './TemplateName';
 
-export default { component: TemplateName, title: 'Pages/TemplateName' };
+export default {
+	component: TemplateName,
+	title: 'Pages/TemplateName',
+	// Learn about controls https://storybook.js.org/docs/react/essentials/controls
+	argTypes: {
+		variant: {
+			options: ['primary', 'secondary'],
+			control: { type: 'radio' },
+		},
+	},
+};
 export const Default = {
 	args: {},
 	render: undefined,
