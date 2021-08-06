a72b82451690dc64cde1e55bf839010920229c55
Zach Bryant <zdb282@gmail.com>
2021-08-05 23:13:25 -0700

    template tweaks, yarn lock

--- src/config/templates/TemplateName.tsx
+++ src/config/templates/TemplateName.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import styles from './TemplateName.module.scss';
 
-
 export interface TemplateNameProps {}
 
-export const TemplateName: React.FC = (props: TemplateNameProps) => (
+export const TemplateName: React.FC<TemplateNameProps> = (props) => (
 	<div className={''} data-testid="TemplateName">
 		TemplateName Component
+		{props}
+		{styles}
 	</div>
 );
 
