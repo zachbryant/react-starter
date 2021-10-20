df70f4175c2eaa95a2881a1ed6169fc225967ef7
Zach Bryant <zdb282@gmail.com>
2021-10-19 21:26:07 -0700

    missed rename

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -4,7 +4,7 @@ export class SimpleStore {
 	}
 
 	static getOrDefault<T = any>(key: string, defaultValue: T): T {
-		const got = Store.get<T>(key);
+		const got = SimpleStore.get<T>(key);
 		if (Object.keys(got).length > 0) return got;
 		return defaultValue;
 	}
